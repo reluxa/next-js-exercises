70bc3628d035209ac78bebe765a4e0e9ba28f313
Peter Bagrij <reluxa@gmail.com>
2024-01-11 12:52:20 +0100

    debug removed

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,6 @@ import {useSession} from "next-auth/react";
 
 export default function About() {
     const { data:session } = useSession()
-    console.log("session", session);
 
     if (session) {
         return (
